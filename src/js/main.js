179eb293316793d54b208df0d384d44d343c1c6d
Mauricio Fernandes Honorato dos Santos <mauhonorat@gmail.com>
2026-08-19 17:22:29 -0300

    organização do css em arquivos separados e carrossel da seção depoimentos concluído.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 
-const secoes = ["hero", "servicos", "sobre", "depoimentos", "rodape"]
+const secoes = ["header", "hero", "servicos", "sobre", "depoimentos", "rodape"]
 
 
 
@@ -24,3 +24,42 @@ async function montarPagina() {
 }
 
 montarPagina()
+
+let indiceAtual = 0;
+
+function mudarSlide(direcao) {
+  const slides = document.querySelectorAll('.box_depoimentos');
+  const totalSlides = slides.length;
+  
+  indiceAtual += direcao;
+  
+  if (indiceAtual >= totalSlides) {
+    indiceAtual = 0;
+  } else if (indiceAtual < 0) {
+    indiceAtual = totalSlides - 1;
+  }
+  
+  const track = document.getElementById('trackDepoimentos');
+  track.style.transform = `translateX(-${indiceAtual * 50}%)`;
+}
+
+// Função para o botão "Ler Mais" no Mobile
+function toggleTexto(botao) {
+  const box = botao.closest('.box_depoimentos');
+  const textoContainer = box.querySelector('.texto_depoimento');
+  
+  if (textoContainer.classList.contains('expandido')) {
+    textoContainer.classList.remove('expandido');
+    botao.textContent = 'Ler mais...';
+  } else {
+    textoContainer.classList.add('expandido');
+    botao.textContent = 'Ler menos';
+  }
+}
+
+
+// Opcional: Passar o slide automaticamente a cada 5 segundos
+
+// setInterval(() => {
+//   mudarSlide(1);
+// }, 5000);
